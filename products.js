76bc758ba20e001802c296ba7467a53a5a79bd0e
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-03-30 15:31:43 -0700

    added intial JS

---
+++ products.js
@@ -0,0 +1,47 @@
+export const monkeys = [
+{
+    name: 'Sheerabert',
+    image: 'orange-monkey.png',
+    size: 'small',
+    category: 'blue',
+    superPower: 'stops panic attacks'
+    isAvailable: true,
+    price: 20,
+},
+{
+    name: 'Luuva',
+    image: 'orange-monkey.png',
+    size: 'medium',
+    category: 'purple',
+    superPower: 'cleans up spills and messes'
+    isAvailable: true,
+    price: 15,
+},
+{
+    name: 'ArgyLynnda',
+    image: 'orange-monkey.png',
+    size: 'medium',
+    category: 'argyle',
+    superPower: 'dances a mean disco'
+    isAvailable: true,
+    price: 25,
+},
+{
+    name: 'Cloakedkent',
+    image: 'orange-monkey.png',
+    size: 'small',
+    category: 'invisible',
+    superPower: 'hides your fears in a distant multiverse'
+    isAvailable: false,
+    price: 120,
+},
+{
+    name: 'Tannerd',
+    image: 'orange-monkey.png',
+    size: 'small',
+    category: 'tan',
+    superPower: 'Sings soothing AMSR sounds'
+    isAvailable: true,
+    price: 20,
+},
+]
