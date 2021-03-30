3cbab3c92602ff778ee0134cdd419afe3b39bafe
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-03-30 16:36:00 -0700

    funtion added

---
+++ utils.js
@@ -0,0 +1,31 @@
+export function monkeyMaker(monkeys) {
+    const li = document.createElement('li');
+    li.classList.add('monkeys');
+    
+    const pName = document.createElement('p');
+    pName.classList.add('name');
+    pName.textContent = monkeys.name;
+    
+    const img = document.createElement('img');
+    img.classList.add('img');
+    img.src = monkeys.image;
+    
+    const pSize = document.createElement('p');
+    pSize.classList.add('size');
+    pSize.textContent = monkeys.size;
+    
+    const pCategory = document.createElement('p');
+    pCategory.classList.add('category');
+    pCategory.textContent = monkeys.category;
+    
+    const superPower = document.createElement('p');
+    superPower.classList.add('superpower');
+    superPower.textContent = monkeys.superPower;
+    
+    const price = document.createElement('p');
+    price.classList.add('price');
+    price.textContent = `$${monkeys.price}.00`;
+
+    li.append(pName, img, pSize, pCategory, superPower, price);
+    return li;
+};
