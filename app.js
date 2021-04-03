9d50919c2e7de04e6ed94f1d1f54aaf56862cf2a
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-04-02 17:17:03 -0700

    index pages rendered

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-// import functions and grab DOM elements
-import { //recently created function }
-import { monkeys } from './products/products.js'
-//grabbing the constant created inside of this path which lists all the current monkeys rendered
-import { monkeyMaker } from './utils.js'
-const ul = document.getElementById('newMonkey');
+// // import functions and grab DOM elements
+// import { //recently created function }
+// import { monkeys } from './products/products.js'
+// //grabbing the constant created inside of this path which lists all the current monkeys rendered
+// import { monkeyMaker } from './utils.js'
+// const ul = document.getElementById('newMonkey');
 
-for(let item of monkeys) {
-    const newLi = monkeyMaker(item);
+// for(let item of monkeys) {
+//     const newLi = monkeyMaker(item);
 
-    ul.append(newLi);
-};
+//     ul.append(newLi);
+// };
 
 
 
