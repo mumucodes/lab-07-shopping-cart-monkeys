0cc0a96d2a7a4dfd77063cd2fe33f130475a565d
mumucodes <75107280+mumucodes@users.noreply.github.com>
2021-04-11 08:22:35 -0700

    newbranch

--- utils.js
+++ utils.js
@@ -1,42 +1,7 @@
 
 import { monkeys } from '../products/products.js';
+import { addItemToCart } from './local-storage-utils.js';
 
-
-export function monkeyMaker(monkeys) {
-    const li = document.createElement('li');
-    li.classList.add('monkeys');
-    
-    const pName = document.createElement('p');
-    pName.classList.add('name');
-    pName.textContent = monkeys.name;
-    
-    const img = document.createElement('img');
-    img.classList.add('img');
-    img.src = '../../assets/' + monkeys.image;
-    
-    const pSize = document.createElement('p');
-    pSize.classList.add('size');
-    pSize.textContent = monkeys.size;
-    
-    const pCategory = document.createElement('p');
-    pCategory.classList.add('category');
-    pCategory.textContent = monkeys.category;
-    
-    const superPower = document.createElement('p');
-    superPower.classList.add('superPower');
-    superPower.textContent = monkeys.superPower;
-    
-    const price = document.createElement('p');
-    price.classList.add('price');
-    price.textContent = `$${monkeys.price}.00`;
-
-    const button = document.createElement('button');
-    button.classList.add();
-    button.textContent = `add to cart`;
-
-    li.append(pName, img, pSize, pCategory, superPower, price, button);
-    return li;
-}
 
 export function findById(array, id) {
     for (let item of array) {
@@ -44,11 +9,71 @@ export function findById(array, id) {
             return item;
         }
     }
+    return null;
 }
 
-export function totalPrice(price, quantity) {
-    return price * quantity;
+
+export function monkeyMaker(monkeys) {
+    const li = document.createElement('li');
+    
+    li.classList.add('monkeys');
+    
+    const pName = document.createElement('p');
+    
+    pName.classList.add('name');
+    pName.textContent = monkeys.name;
+    
+    const img = document.createElement('img');
+    
+    img.classList.add('img');
+    img.src = '../../assets/' + monkeys.image;
+    
+    const pSize = document.createElement('p');
+    
+    pSize.classList.add('size');
+    pSize.textContent = monkeys.size;
+    
+    const pCategory = document.createElement('p');
+    
+    pCategory.classList.add('category');
+    pCategory.textContent = monkeys.category;
+    
+    const superPower = document.createElement('p');
+    
+    superPower.classList.add('superPower');
+    superPower.textContent = monkeys.superPower;
+    
+    const price = document.createElement('p');
+    
+    price.textContent = monkeys.price.toLocalString('en-US', {
+        style: 'currency'
+        currency: 'USD',
+
+    });
+
+    // }`$${monkeys.price}.00`;
+    // price.classList.add('price');
+    
+
+    const button = document.createElement('button');
+    
+    button.textContent = `add to cart`;
+    
+    button.addEventListener('click', () => {
+        addItemToCart(monkeys.id);
+    });
+
+    // button.classList.add();
+
+    
+    li.append(pName, img, pSize, pCategory, superPower, price, button);
+    return li;
 }
+
+
+// export function totalPrice(price, quantity) {
+//     return price * quantity;
+// }
 
 export function createRowCart(cartItem, monkeys) {
     const tr = document.createElement('tr');
